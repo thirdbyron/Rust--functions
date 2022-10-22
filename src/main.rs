10d1263c62dc512da89c2e18d43c9bc7c786029b
thirdbyron <rgnatovsky@gmail.com>
2022-10-22 10:28:28 +0300

    Function with parameter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5);
 }
 
-fn another_function() {
-    println!("Hello from another function!")
+fn another_function(x: u32) {
+    println!("Hello from another function! This is a parameter - {x}")
 }
